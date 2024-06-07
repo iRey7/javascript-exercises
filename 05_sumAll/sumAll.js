61369dc5b660354788f51b8fa7165068692467af
Santos Reyes <sreyesx23@gmail.com>
2024-06-07 13:32:29 -0400

    Filter out negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,19 @@
 const sumAll = function(a, b) {
     let total = 0;
     let array = [];
-    if (a <= b) {
+    if (a < 0) {
+        return 'ERROR';
+    }
+    else if (a <= b && a > 0) {
         for (let i = a; i <= b; ++i) {
             array.push(i);
         }
     }
-    if (b <= a) {
+
+    if (b < 0) {
+        return 'ERROR';
+    }
+    else if (b <= a && b > 0) {
         for (let i = b; i <= a; ++i) {
             array.push(i);
         }
