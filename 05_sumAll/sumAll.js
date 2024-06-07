f6f6188b27264f4d29813aca95f9251509b9de29
Santos Reyes <sreyesx23@gmail.com>
2024-06-07 13:04:15 -0400

    Create a sum function adding array items

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = 0;
+    let array = [];
+    if (a <= b) {
+        for (let i = a; i <= b; ++i) {
+            array.push(i);
+        }
+    }
+    array.forEach(myFunction);
+    function myFunction(item) {
+        total += item;
+    }
+    return total;
 };
 
 // Do not edit below this line
