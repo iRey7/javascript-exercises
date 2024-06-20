1184d72fc4846ed2f2bf481da32712f87ebe507e
Santos Reyes <sreyesx23@gmail.com>
2024-06-20 11:51:37 -0400

    pass first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f - 32) * 5 / 9;
+  if (celsius === 0) {
+    return celsius;
+  }
+  else {
+    return celsius.toFixed(1);
+  }
+  //let roundedNumber = celsius.toFixed(1);
+
+  //return roundedNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = (c * 9 / 5 + 32);
+  let roundedNumber = fahrenheit.toFixed(1);
+
+  return roundedNumber;
 };
 
 // Do not edit below this line
